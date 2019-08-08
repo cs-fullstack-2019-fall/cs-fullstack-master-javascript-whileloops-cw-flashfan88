cf523f7344059af2bfbc8180377376869e1d88c3
flashfan88 <lavarjohnson20@gmail.com>
2019-08-08 16:58:49 -0500

    moregradedwork

---
+++ index2.js
@@ -0,0 +1,48 @@
+//Exercise 1
+//Print -10 through 5 using a counter and while loop.
+
+// var counter = -10;
+//
+// while (counter <=5)
+//     {
+//         console.log(counter);
+//         counter = counter + 1;
+//     }
+//
+//
+//Exercise 2
+// Write a While loop that counts backwards from 10 to 0 Log the current count at the end of each iteration EXCEPT if the current count is 5
+
+
+var number = 10;
+
+while(number >=0)
+{
+    if (number === 5)
+    {
+
+    }
+    console.log(number);
+    number = number - 1;
+}
+
+
+
+//Exercise 3
+//Asks the user Enter a word. Enter 'q' to quit.
+//Create a while loop that will not quit until they enter 'q'.
+
+
+var userInput = prompt("Enter anything");
+var input = false;
+
+while (userInput === q)
+{
+    if (userInput != q)
+    {
+
+    }
+}
+
+
+
